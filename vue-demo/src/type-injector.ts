0e14af063d3efb3de8519f5dca431e37bce7c3d3
alexandrustefanescu <stefanescualexandru1994@gmail.com>
2022-10-07 20:26:09 +0200

    create type injector file
    remove unnecessary files from project

---
+++ vue-demo/src/type-injector.ts
@@ -0,0 +1,12 @@
+import { TypeInjector, Logger } from '@type-injector/lib';
+import { InfoLogger, injectToken } from 'type-injector-lib-demo-common-api';
+
+export const injector = TypeInjector.construct()
+  .provideImplementation(Logger, InfoLogger)
+  .provideValue(injectToken.simpleValue, 'Hello web component!')
+  .provideFactory(injectToken.createdValue, {
+    deps: [injectToken.simpleValue],
+    create: (greeter) =>
+      `${greeter} Time is: ${new Date().toLocaleTimeString()}`,
+  })
+  .build();
