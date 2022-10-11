866bf4d762efcac79a3b7a91b43528b627727ced
Alexandru Stefanescu <alexandru@stefanescu@dekra.com>
2022-10-11 23:20:11 +0200

    update Header component and styling

--- preact-demo/src/components/header/index.tsx
+++ preact-demo/src/components/header/index.tsx
@@ -1,16 +1,13 @@
-import { h } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style.css';
+import { h } from 'preact'
+import IntegrationVariants from '../integration-variants'
+import style from './style.css'
 
 const Header = () => (
 	<header class={style.header}>
 		<h1>Preact App</h1>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
+		<img class='logo' src='../../assets/logo.svg' alt='Preact Logo' />
+		<IntegrationVariants />
 	</header>
-);
+)
 
-export default Header;
+export default Header
